337d1107ddc8d222474f2c75a1eb5464c52278d2
WhoIsMeh <ellisbro58@hotmail.co.uk>
2021-09-18 13:40:12 +0100

    Update NumberNames.lua
    Fixed indentation - not sure what caused this

--- NumberNames.lua
+++ NumberNames.lua
@@ -1,13 +1,13 @@
 local wordsDigits = {
-	
+
 	"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
 	"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
-	
+
 }
 local wordsTens = {
-	
+
 	"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
-	
+
 }
 
 function numToWord(num)
@@ -20,45 +20,45 @@ function numToWord(num)
 
 	if thousands > 0 then
 
-	    string = string .. numToWord(thousands) .. " thousand "
-		
+		string = string .. numToWord(thousands) .. " thousand "
+
 	end
-		
+
 	if hundreds > 0 then
-	   
-	    string = string .. wordsDigits[hundreds] .. " hundred" .. (flat > 0 and " and")
-	   
+
+		string = string .. wordsDigits[hundreds] .. " hundred" .. (flat > 0 and " and")
+
 	end
 
 	if flat > 0 then
-	   
-	    if string.Length ~= 0 then
-		   
-		    string = string .. " "
-		   
-	    end
-	   
-	    if flat < 20 then
-		   
-		    string = string .. wordsDigits[flat]
-		   
-	    else
-		   
-		    local tens = tonumber(string.sub(flat, 1, 1))
-		    local units = tonumber(string.sub(flat, 2, 2))
-		   
-		    string = string .. wordsTens[tens - 1]
-		   
-		    if units > 0 then
-			   
-			    string = string .. "-" .. wordsDigits[units]
-		   
-		    end
-		   
-	    end
-	   
- 	end
+
+		if string.Length ~= 0 then
+
+			string = string .. " "
+
+		end
+
+		if flat < 20 then
+
+			string = string .. wordsDigits[flat]
+
+		else
+
+			local tens = tonumber(string.sub(flat, 1, 1))
+			local units = tonumber(string.sub(flat, 2, 2))
+
+			string = string .. wordsTens[tens - 1]
+
+			if units > 0 then
+
+				string = string .. "-" .. wordsDigits[units]
+
+			end
+
+		end
+
+	end
 
 	return(string)
-   
+
 end

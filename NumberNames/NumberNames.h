f63bee9b16159775f2dced9fd856e8e517b6a5be
WhoIsMeh <ellisbro58@hotmail.co.uk>
2021-09-18 15:07:03 +0100

    Update NumberNames.h
    Fixed header file by adding namespace declaration

--- NumberNames/NumberNames.h
+++ NumberNames/NumberNames.h
@@ -1,48 +1,59 @@
+#ifndef NUMBER_NAME_H
+#define NUMBER_NAME_H
+
+#include <iostream>
 #include <math.h>
 using namespace std;
 
-string wordsDigits[20] = {
+const string WORDS_DIGITS[20] = {
 	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
 	"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
 };
-string wordsTens[8] = {
+const string WORDS_TENS[8] = {
 	"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
 };
 
-string numberName(int num)
+namespace numName
 {
 
-	int thou = floor(num / 1000);
-	int hund = floor(num / 100 % 10);
-	int flat = num % 100;
+	string numberName(int num)
+	{
 
-	string result = "";
-	if (thou > 0)
-	{
-		result = numberName(thou) + " thousand" + (num - thou * 1000 > 0 ? " " : "");
-	}
-	if (hund > 0)
-	{
-		result = result + wordsDigits[hund] + " hundred" + (flat > 0 ? " and " : "");
-	}
-	if (flat > 0)
-	{
-		if (flat < 20) 
+		int thou = floor(num / 1000);
+		int hund = floor(num / 100 % 10);
+		int flat = num % 100;
+
+		string result = "";
+		if (thou > 0)
 		{
-			result = result + wordsDigits[flat];
+			result = numberName(thou) + " thousand" + (num - thou * 1000 > 0 ? " " : "");
 		}
-		else
+		if (hund > 0)
 		{
-			int tens = floor(flat / 10);
-			int units = flat % 10;
-			result = result + wordsTens[tens - 2];
-
-			if (units > 0)
+			result = result + WORDS_DIGITS[hund] + " hundred" + (flat > 0 ? " and " : "");
+		}
+		if (flat > 0)
+		{
+			if (flat < 20)
 			{
-				result = result + "-" + wordsDigits[units];
+				result = result + WORDS_DIGITS[flat];
+			}
+			else
+			{
+				int tens = floor(flat / 10);
+				int units = flat % 10;
+				result = result + WORDS_TENS[tens - 2];
+
+				if (units > 0)
+				{
+					result = result + "-" + WORDS_DIGITS[units];
+				}
 			}
 		}
-	}
-	return result;
+		return result;
 
-};
+	};
+
+}
+
+#endif

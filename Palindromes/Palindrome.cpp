5c75aa3863e56f94250b828855dcd21c1f0eb310
Josh Ellis <ellisbro58@hotmail.co.uk>
2021-09-22 09:40:25 +0100

    Update Palindrome.cpp
    Changed tab width for consistency

--- Palindromes/Palindrome.cpp
+++ Palindromes/Palindrome.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    std::string input;
-    std::cout << "Please enter a word to verify if it is a palindrome.";
-    std::cin >> input;
-    std::cout << input << (input == std::string(input.rbegin(), input.rend()) ? " is": " is not") << " a palindrome";
+	std::string input;
+	std::cout << "Please enter a word to verify if it is a palindrome.";
+	std::cin >> input;
+	std::cout << input << (input == std::string(input.rbegin(), input.rend()) ? " is": " is not") << " a palindrome";
 }
